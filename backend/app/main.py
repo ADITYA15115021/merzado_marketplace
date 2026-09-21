f2baf1be99b32f8a21280179228cefa2bb0b0264
ADITYA KAMBOJ <sierra.phoenix1511@gmail.com>
2026-09-21 13:02:16 +0530

    added cors for vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+    "http://localhost:5173",
+    "https://merzado-marketplace.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
